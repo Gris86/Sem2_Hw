55abb6ba9e531a9c14aa3541ae6cfe22aaa3b62a
Snake <stop@mail.ru>
2023-07-26 20:24:12 +0300

    task13_math

---
+++ task13/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+int num=new Random().Next(0,100000);
+if(num < 99) Console.WriteLine("В данном числе нет третьей цифры.");
+else {
+    int num2=num;
+    while (num>1000)
+        num=num/10;
+Console.WriteLine($"Третья цифра числа {num2} это {num%10}");     
+}
