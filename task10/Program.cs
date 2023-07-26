03b5fdcf7608ab674b477a386c2e17e0cc2fdc30
Snake <stop@mail.ru>
2023-07-26 20:17:43 +0300

    task10_string

--- task10/Program.cs
+++ task10/Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-int number = new Random().Next(100,1000);
-int secondNum=(number/10)%10;
-Console.WriteLine($"Вторая цифра числа {number} это {secondNum}");
+Console.WriteLine("Введите трехзначное число");
+string number = Console.ReadLine();
+if(number.Length!=3 && number[0]!=0) Console.WriteLine("Данное число не трехзначное");
+else Console.WriteLine($"Второе число числа {number} это {number[1]}");
