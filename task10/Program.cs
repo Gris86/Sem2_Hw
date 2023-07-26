fddd2a62606a8472c4bf35e4b5906f2585642113
Snake <stop@mail.ru>
2023-07-26 20:16:46 +0300

    task10_math

---
+++ task10/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+int number = new Random().Next(100,1000);
+int secondNum=(number/10)%10;
+Console.WriteLine($"Вторая цифра числа {number} это {secondNum}");
